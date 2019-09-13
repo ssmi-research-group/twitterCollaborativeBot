4dd10e976a830ced6f2fc9dcf5e89aa92f61ad97
Lucas Rotsen Pereira <lucasrotsen@gmail.com>
2019-09-13 14:51:02 -0300

    Uploading twitter configuration file

---
+++ configuration/twitter_connection.py
@@ -0,0 +1,35 @@
+import twitter
+from twitter import error
+
+consumerKey = ''
+consumerSecret = ''
+accessToken = ''
+accessTokenSecret = ''
+
+
+def open_connection():
+    if are_keys_valid():
+        try:
+            api = twitter.Api(consumer_key=consumerKey,
+                              consumer_secret=consumerSecret,
+                              access_token_key=accessToken,
+                              access_token_secret=accessTokenSecret,
+                              sleep_on_rate_limit=True)
+
+            return api
+
+        except error.TwitterError as e:
+            print("Erro ao se conectar: " + str(e.message))
+            return None
+
+    else:
+        raise ValueError('Todos as chaves devem ser corretamente preenchidas antes de executar algoritmo.')
+
+
+def are_keys_valid() -> bool:
+    are_valid = True
+
+    if consumerKey == '' or consumerSecret == '' or accessToken == '' or accessTokenSecret == '':
+        are_valid = False
+
+    return are_valid
--- whoknowsbot-config.sh
+++ whoknowsbot-config.sh
@@ -5,14 +5,29 @@ then
 	echo "A versão do UBUNTU deve ser 18.04!.."
 else
 	echo "Iniciando atualização de pacotes..."
-	sudo apt-get update
+	sudo apt update
 
 	echo "Iniciando instalação do Python3.6..."
-	sudo apt-get install python3.6
+	sudo apt install python3.6
 
 	echo "Iniciando instalação do gerenciador de pacotes pip..."
-	sudo apt-get install python3-pip
+	sudo apt install python3-pip
 
 	echo "Iniciando instalação das dependências do projeto..."
-	sudo python3 -m pip install setuptools python-twitter nltk wordcloud backoff
+
+	echo "Iniciando instalação do setuptools..."
+	sudo python3 -m pip install setuptools
+
+	echo "Iniciando instalação do python-twitter..."
+	sudo python3 -m pip install python-twitter
+
+	echo "Iniciando instalação do nltk..."
+	sudo python3 -m pip install nltk
+
+	echo "Iniciando instalação do wordcloud..."
+	sudo python3 -m pip install wordcloud
+
+	echo "Iniciando instalação do backoff..."
+	sudo python3 -m pip install backoff
+
 fi
