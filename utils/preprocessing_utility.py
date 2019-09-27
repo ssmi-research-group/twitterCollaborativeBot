8ead75112d8f58b36ea6418a8d682516c6375eb2
Lucas Rotsen Pereira <lucasrotsen@gmail.com>
2019-09-26 23:33:26 -0300

    Removing useless comments

--- utils/preprocessing_utility.py
+++ utils/preprocessing_utility.py
@@ -1,53 +1,6 @@
 import re
 import string
 
-"""
-1 - @WhoKnowsBot QuEm SaBE,             banana?
-2 - @WhoKnowsBot QuEm SaBE             banana
-3 - @WhoKnowsBot QuEm SaBE banana
-4 - ['@WhoKnowsBot', QuEm, SaBE, banana] <- ERRADO
-5 - ['@WhoKnowsBot', QuEmSaBE, banana]
-
-1 - @WhoKnowsBot QuEm SaBE,             banana? macaco tesouro beliche
-2 - QuEm SaBE,             banana? macaco tesouro beliche
-3 - QuEm SaBE             banana macaco tesouro beliche
-4 - QuEm SaBE banana macaco tesouro beliche
-    sobre o que sabem banana macaco tesouro beliche
-    quantos sabem banana macaco tesouro beliche
-
-    quemsabe banana macaco tesouro beliche
-    sobreoquesabem banana macaco tesouro beliche
-    quantossabem banana macaco tesouro beliche
-
-5 - banana macaco tesouro beliche
-6 - ['banana', 'macaco', 'tesouro', 'beliche']
-
-1 - @WhoKnowsBot
-2 - ''
-3 - ''
-4 - ''
-5 - (None, None)
-
-1 - @WhoKnowsBot banana
-2 - banana
-3 - banana
-4 - banana
-5 - (None, None)
-
-1 - @WhoKnowsBot QUEMSABE
-2 - QUEMSABE
-3 - QUEMSABE
-4 - QUEMSABE
-5 - 
-
-1 - @WhoKnowsBot QUEM SABEcorrida de                 carros
-2 - QUEM SABEcorrida de                 carros
-3 - QUEM SABEcorrida de carros
-4 - (QUEM SABE, corrida)
-5 -
-
-"""
-
 def tweetPreprocessing (tweet_text):
 
     prepTweet = removeRedundantSpaces(removePunctuation(removeBotAccount(tweet_text)))
